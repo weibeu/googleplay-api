f45b2d7a7bda1ac44e7eb2293e4751455ee2a3fc
Rehmat Alam <contact@rehmat.works>
2020-04-21 22:09:18 +0500

    Customized for gplaydl CLI tool

--- setup.py
+++ setup.py
@@ -1,48 +1,14 @@
 from setuptools import setup
-from setuptools.command.build_py import build_py as _build
 
-
-import os.path
-import subprocess
-import shutil
-
-PROTOC_EXEC = "protoc"
-
-CURRENT_DIR = os.path.abspath( os.path.dirname( __file__ ) )
-
-class ProtobufBuilder(_build):
-
-    def run(self):
-        # check if protobuf is installed
-        exec_path = shutil.which(PROTOC_EXEC)
-        if exec_path is None:
-            raise Exception("You should install protobuf compiler")
-
-        print("Building protobuf file")
-        subprocess.run([exec_path,
-            "--proto_path=" + CURRENT_DIR,
-            "--python_out=" + CURRENT_DIR + "/gpapi/",
-            CURRENT_DIR + "/googleplay.proto"])
-        super().run()
-
-setup(name='gpapi',
-      version='0.4.4',
+setup(name='gpapidl',
+      version='1.0.1',
       description='Unofficial python api for google play',
-      url='https://github.com/NoMore201/googleplay-api',
-      author='NoMore201',
-      author_email='domenico.iezzi.201@gmail.com',
+      url='https://github.com/rehmatworks/googleplay-api',
+      author='Rehmat Alam',
+      author_email='contact@rehmat.works',
       license='GPL3',
-      packages=['gpapi'],
-      package_data={
-          'gpapi': [
-              'config.py'
-              'device.properties',
-              'googleplay_pb2.py',
-              'googleplay.py',
-              'utils.py'
-          ]},
-      include_package_data=True,
-      cmdclass={'build_py': ProtobufBuilder},
+      packages=['gpapidl'],
+      package_data={'gpapidl': ['device.properties']},
       install_requires=['cryptography>=2.2',
                         'protobuf>=3.5.2',
                         'requests'])
